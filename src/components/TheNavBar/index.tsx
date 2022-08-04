48ebbe062bfee7f55d4719483792af94859e6ad1
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-08-04 13:02:14 -0300

    creating the nav bar components and converting style files extensions from js to ts

--- src/components/TheNavBar/index.tsx
+++ src/components/TheNavBar/index.tsx
@@ -1,1 +1,10 @@
-export const TheNavBar: React.FC = () => <h1>NavBar</h1>
+import { NavBar } from './styles'
+
+import Pagcom_logo from '../../assets/images/Pagcom_logo.png'
+
+export const TheNavBar: React.FC = () => 
+    
+    <NavBar>
+        <p>Seja um agente da </p>
+        <img src={Pagcom_logo} alt="Pagcom logo" />
+    </NavBar>
