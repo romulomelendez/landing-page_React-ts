ac5ab8659b993e4498832d80b9b77a6c22ac25c4
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-08-04 21:04:40 -0300

    creating a globalstyle with styled-components lib

--- src/components/TheNavBar/index.tsx
+++ src/components/TheNavBar/index.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from 'react'
+
 import { NavBar, Text, MiniContainer, Img } from './styles'
 
 import Pagcom_logo from '../../assets/images/Pagcom_logo.png'
 
-export const TheNavBar: React.FC = () => 
+export const TheNavBar: React.FC = () => {
     
-    <NavBar>
-        <MiniContainer>
-            <Text>Seja um agente da </Text>
-            <Img src={Pagcom_logo} alt="Pagcom logo" height="32" />
-        </MiniContainer>
-    </NavBar>
+    useEffect(() => {
+        fetch(import.meta.env.VITE_BASE_API_URL + 'posts')
+         .then( response => response.json() )
+         .then( data => console.log(data) )
+        
+    }, [])
+    
+    
+    return (
+    
+        <NavBar>
+            <MiniContainer>
+                <Text>Seja um agente da </Text>
+                <Img src={Pagcom_logo} alt="Pagcom logo" height="32" />
+            </MiniContainer>
+        </NavBar>
+
+    )
+
+}
