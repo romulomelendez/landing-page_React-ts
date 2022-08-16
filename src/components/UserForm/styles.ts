01f016ccd07c59281e20345d548df392d73e0570
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-08-16 08:49:13 -0300

    margin update

--- src/components/UserForm/styles.ts
+++ src/components/UserForm/styles.ts
@@ -6,6 +6,7 @@ export const Container = styled.div `
     grid-template-columns: repeat(2, 50%);
     height: max-content;
     /* margin-top: 100px; */
+    margin-bottom: 100px;
     column-gap: 25px;
 
     @media (max-width: 1484px) {
