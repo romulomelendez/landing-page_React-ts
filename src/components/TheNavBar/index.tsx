a55a6c279e6d4d8c21bb465e43e96fb0b6d73282
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-08-16 07:57:57 -0300

    dinamic text in NavBar component

--- src/components/TheNavBar/index.tsx
+++ src/components/TheNavBar/index.tsx
@@ -1,24 +1,20 @@
-// import { useEffect } from 'react'
+import { useContext } from 'react'
+
+import { PageContext } from '../../contexts/PageContext'
 
 import { NavBar, Text, MiniContainer, Img } from './styles'
 
 import Pagcom_logo from '../../assets/images/Pagcom_logo.png'
 
 export const TheNavBar: React.FC = () => {
-    
-    // useEffect(() => {
-    //     fetch(import.meta.env.VITE_BASE_API_URL + 'posts')
-    //      .then( response => response.json() )
-    //      .then( data => console.log(data) )
-        
-    // }, [])
-    
+
+    const { titles } = useContext(PageContext)
     
     return (
     
         <NavBar>
             <MiniContainer>
-                <Text>Seja um agente da </Text>
+                <Text>{ titles[0] }</Text>
                 <Img src={Pagcom_logo} alt="Pagcom logo" height="32" />
             </MiniContainer>
         </NavBar>
